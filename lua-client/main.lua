d11f3651f21fa14af208932acd596a61ddc78748
ccastrom <claudiocastrom7@gmail.com>
2025-05-03 17:39:07 -0400

    feat: add counter and while loop to simulate continuous data transmission on 'main.lua'

--- lua-client/main.lua
+++ lua-client/main.lua
@@ -3,14 +3,21 @@ local socket = require("socket");
 
 local client = socket.connect("127.0.0.1", 4000)
 
-
+local count=0
 if client then
     print("Connection success")
 
-    socket.sleep(1)
-    client:send("1")
+    while true do
+        count=count+1
+        socket.sleep(1.5)
+        client:send(count)
 
-    client:close()
+
+    end
+
+ 
+
+    --client:close()
 
    
     
